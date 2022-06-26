86a0b574ff3ada310d13d7bde665092bb5daae17
Miguel Ambrosio <miguel120820ad@gmail.com>
2022-06-26 16:46:43 -0500

    Se modificaron alos archivos

---
+++ tablas.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+<div class="container" id="registration-form">
+<div id="contenedorTabla">
+        <div class="header">
+            <h2>Prestamos</h2>
+        </div>
+        <div id="prestamo">
+        </div>
+        <br>
+
+        <br>
+        <div class="header">
+            <h2>Amortizaciones</h2>
+        </div>
+        <div id="amortizaciones">
+        </div>
+    </div>
+</div>
+@endsection
